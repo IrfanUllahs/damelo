9ad294ec7bd97d9c36e34e2848db6a8deeaef100
irfan <irfan>
2024-09-11 18:32:06 +0500

    complete the search page and also modified the dashboard page

--- app/video/[path]/page.js
+++ app/video/[path]/page.js
@@ -1,14 +1,17 @@
 "use client";
 import React from "react";
-import ReactPlayer from "react-player/lazy";
 
 function page() {
   return (
-    <div className="flex items-center justify-center pt-5">
-      <ReactPlayer
-        url="https://www.youtube.com/watch?v=zumJJUL_ruM&pp=ygUNNGsgaG9tZSB2aWRlbw%3D%3D"
-        height={550}
-      />
+    <div className="flex justify-center pt-5">
+      <video width="800" height="500" controls>
+        <source
+          src="https://st3v.idealista.com/36/1b/d6/hd_1241347812.mp4"
+          type="video/mp4"
+        />
+        Error Message
+      </video>
+      {/* <Video source="https://www.youtube.com/watch?v=0S-pyTJ2ZvU&pp=ygUOZm9ybWlrIHJlYWN0anM%3D" /> */}
     </div>
   );
 }
